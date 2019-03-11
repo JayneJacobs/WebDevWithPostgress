4959ea0c18856c41968d26846cd541e4ec0cc8ad
jjacob151 <jayne_jacobs@cable.comcast.com>
2019-03-11 17:51:00 -0400

    Adding pprof

--- songsAppTempl/main.go
+++ songsAppTempl/main.go
@@ -23,8 +23,8 @@ func main() {
 	db := connectToDatabase()
 	defer db.Close()
 	controller.Startup(templates)
-	go http.ListenAndServe(":8070", nil)
-	http.ListenAndServeTLS(":8070", "cert.pem", "key.pem", &middleware.TimeoutMiddleware{new(middleware.GzipMiddleware)})
+	go http.ListenAndServe(":8080", nil)
+	http.ListenAndServeTLS(":8000", "cert.pem", "key.pem", &middleware.TimeoutMiddleware{new(middleware.GzipMiddleware)})
 
 }
 
